e7a567ad70108da57e7ccec5b5c0b3dbe770029e
Peter <peter@curlybyte.com>
2021-05-13 17:25:14 +0200

    update 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-container class="grey lighten-5 mb-6">
+      <v-row
+        :align="align"
+        no-gutters
+        style="height: 150px;"
+      >
+        <v-col
+          v-for="n in 4"
+          :key="n"
+          cols="12"
+          xs="12"
+          sm="6"
+          md="6"
+          lg="3"
+          xl="3"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            A card
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    data: () => ({
+     
+    }),
   }
-}
 </script>
